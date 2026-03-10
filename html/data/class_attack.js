4f57c5367b8d715dbcd0d675b1077633818b1a63
titou_qlvr <titouan.quillevere@gmail.com>
2026-03-10 15:11:36 +0100

    ca me semble bien comme methode

--- html/data/class_attack.js
+++ html/data/class_attack.js
@@ -2,12 +2,14 @@ class Attack{
 
     static all_attacks = []
 
-    constructor(id_attack, name, type, power, duration){
+    constructor(id_attack, name, type, power, duration, fast = 0, charged = 0){
         this.id_attack = id_attack
         this.name = name
         this.type = type
         this.power = power
         this.duration = duration
+        this.fast = fast
+        this.charged = charged
     }
 
     toString() {
@@ -16,18 +18,13 @@ class Attack{
 
     fill_attacks() {
         fast_moves.forEach(element => {
-            let attack = new Attack(element['move_id'],element['name'],element['type'],element['power'],element['duration'])
+            let attack = new Attack(element['move_id'],element['name'],element['type'],element['power'],element['duration'],fast = 1)
             all_attacks.push(attack)
         })
 
         charged_moves.forEach(element => {
-            let attack = new Attack(element['move_id'],element['name'],element['type'],element['power'],element['duration'])
+            let attack = new Attack(element['move_id'],element['name'],element['type'],element['power'],element['duration'], charged = 1)
             all_attacks.push(attack)
         })
-
-        // pokemon_moves.forEach(element => {
-        //     let attack = new Attack(element['move_id'],element['name'],element['type'],element['power'],element['duration'])
-        //     all_attacks.push(attack)
-        // })
     }
 }
