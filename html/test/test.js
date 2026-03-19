92f7353393f5f8ec2fac746f8786856b55fa52af
mathislqs <mathisloquais@gmail.com>
2026-03-19 11:45:21 +0100

    Update test.js

---
+++ html/test/test.js
@@ -0,0 +1,35 @@
+
+function getPokemonsByType(typeName){
+    //TODO affichant, dans la console, la liste des Pokémons pour un type donné (en argument).
+}
+
+function getPokemonsByAttack(attackName){
+    //TODO affichant, dans la console, la liste des Pokémons pour une attaque donnée (en argument)
+}
+
+function getAttacksByType(typeName) {
+    //TODO affichant, dans la console, la liste des attaques pour un type donné (en argument).
+}
+
+function sortPokemonByTypeThenName(){
+    //TODO affichant, dans la console, la liste des Pokémons triés par type puis par nom dans l’ordre alphabétique.
+}
+
+function getWeakestEnemies(attackName){
+    //TODO de la classe Pokemon, affichant, dans la
+    // console, la liste des Pokémons pour lesquels l’attaque choisie est la plus efficace. Le nom
+    // de l’attaque est une chaîne de caractères.
+
+}
+
+function getBestFastAttacksForEnemy(print, pokemonName){
+    // TODO affichant, dans la
+    // console et uniquement si le paramètre print vaut true, la liste des attaques
+    // (toString()) et de leurs dégâts (Puissance x Efficacité x (Base Attack A / Base
+    // Attack B)) contre un Pokémon donné.
+}
+
+function fastFight(pokemonNameA, pokemonNameB){
+    //TODO affichant, dans la console, le
+    // déroulement d’un combat entre 2 pokemons. Ici vous devez utiliser console.table().   
+}
